c3044f865ee6901aab7b2bc942d32f3c1d86d530
Liudmila <lohmataja@gmail.com>
2014-09-08 16:11:48 -0400

    Finished implementing all functions.

--- phonebooks/phonebook.py
+++ phonebooks/phonebook.py
@@ -1,72 +1,67 @@
 __author__ = 'Liuda'
-import os, sys
 import sqlite3
+import sys
 
-def exists(entry, field, db, cursor):
-    return cursor.execute("SELECT * FROM {} WHERE {}=? LIMIT 1".format(db, field), (entry, )).fetchone() != None
+def pb_id(cursor, phonebook):
+    return cursor.execute("SELECT ROWID FROM pb_registry WHERE name=?", (phonebook, )).fetchone()[0]
 
 def create(cursor, phonebook):
     cursor.execute('CREATE TABLE IF NOT EXISTS pb_data (name TEXT, phone_number TEXT, pb_id INTEGER, UNIQUE (name, pb_id) ON CONFLICT FAIL )')
     cursor.execute('CREATE TABLE IF NOT EXISTS pb_registry (name TEXT, UNIQUE (name) ON CONFLICT FAIL )')
     try:
         cursor.execute("INSERT INTO pb_registry VALUES (?)", (phonebook, ))
-        print('{} created!'.format(phonebook))
+        print("{} has been created.".format(phonebook))
     except sqlite3.IntegrityError:
-        print('Error: {} already exists!'.format(phonebook))
+        print("{} already exists".format(phonebook))
 
 def add(cursor, name, phone_number, phonebook):
-    #check that phonebook exists
-    if not exists(phonebook, 'pb_name', 'pb_registry', cursor):
-        user_input = input("{} doesn't exist. Would you like to create it? (Y to create)".format(phonebook)).lower()
-        if user_input == 'y':
-            create(cursor, phonebook)
-        else:
-            print("Command cancelled")
-            return
-    #check whether entry exists
-    if not exists(name, 'name', phonebook, cursor):
-        cursor.execute("INSERT INTO pb_data VALUES (?, ?, ?)", (name, phone_number, phonebook))
-        print('Added {} to {}'.format(name, phonebook))
-        return True
-    else:
-        print('Error: {} already exists in {}. Use the "update" command.'.format(name, phonebook))
-        return False
-
-def update(name, phone_number, filename, phonebook):
-    print('Updated an entry')
     try:
-        print('Previous entry:')
-        print(name, phonebook[name])
-    except:
-        print('Previous entry not found.')
-    phonebook[name] = phone_number
-    print('New entry:')
-    print(name+'\t'+phonebook[name])
+        cursor.execute("INSERT INTO pb_data VALUES (?, ?, ?)", (name, phone_number, pb_id(cursor, phonebook)))
+        print("Added an entry to {}\n{}\t{}".format(phonebook, name, phone_number))
+    except sqlite3.IntegrityError:
+        print("{} already exists in {}. Use the 'update' command.".format(name, phonebook))
 
-def lookup(cursor, name, phonebook, field='name'):
-    if exists(name, 'name', phonebook, cursor):
-        phone_number = cursor.execute("SELECT * FROM {} WHERE {} IN '{}'".format(
-            phonebook, field, name)).fetchall()
-        for p in phone_number:
-            print(p)
-        # print("{}: {}".format(phone_number))
+def lookup(cursor, name, phonebook):
+    rows = cursor.execute("SELECT name, phone_number FROM pb_data WHERE name LIKE ? and pb_id=?",
+                                       ('%'+name+'%', pb_id(cursor, phonebook))).fetchall()
+    if rows:
+        print('\n'.join('\t'.join(row) for row in rows))
     else:
-        print("Error: entry {} not found in {|".format(name, phonebook))
+        print("Error: {} does not exist in {}".format(name, phonebook))
 
-def reverse_lookup(cursor, name, phonebook):
-    lookup(cursor, name, phonebook, field='phone_number')
+def reverse_lookup(cursor, phone_number, phonebook):
+    name = cursor.execute("SELECT name, phone_number FROM pb_data WHERE phone_number=? AND pb_id=?",
+                               (phone_number, pb_id(cursor, phonebook))).fetchone()
+    if name:
+        print('\t'.join(name))
+    else:
+        print('Error: {} does not exist in {}'.format(phone_number, phonebook))
 
 def remove(cursor, name, phonebook):
-    # if exists(name, 'name', 'pb_data')
-    pass
+    try:
+        cursor.execute("DELETE FROM pb_data WHERE name=? AND pb_id=?", (name, pb_id(cursor, phonebook)))
+        print("Removed {} from {}".format(name, phonebook))
+    except sqlite3.OperationalError:
+        return "Error: {} does not exist in {}".format(name, phonebook)
+
+def update(cursor, name, new_phone, phonebook):
+    old_phone = cursor.execute("SELECT phone_number FROM pb_data WHERE name=? AND pb_id=? LIMIT 1",
+                               (name, pb_id(cursor, phonebook))).fetchone()[0]
+    if old_phone:
+        cursor.execute("UPDATE pb_data SET phone_number=? where name=? and pb_id=?",
+            (new_phone, name, pb_id(cursor, phonebook)))
+        print("Updated an entry for {} in {}.\nOld phone #: {}\tNew phone #: {}".format(
+            name, phonebook, old_phone, new_phone))
+    else:
+        print("Error: {} does not exist in {}".format(name, phonebook))
 
 if __name__ == '__main__':
     #parse arguments
     args = sys.argv[1:]
     command = args.pop(0)
-    #open the database
     db_conn = sqlite3.connect('./phonebook.db')
     cursor = db_conn.cursor()
+
     #dispatch the command
     dispatch = {'create':create,
                 'add':add,
@@ -75,9 +70,6 @@ if __name__ == '__main__':
                 'reverse_lookup':reverse_lookup,
                 'remove':remove}
     dispatch[command](cursor, *args)
-    #save changes, exit
+
     db_conn.commit()
     db_conn.close()
-
-# db_conn = sqlite3.connect('./phonebook.db')
-# c = db_conn.cursor()
